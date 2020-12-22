9ed5dce982d62b51d22cfa27da5cdf2d266f03f5
saiashish9 <saiashish3760@gmail.com>
2020-12-23 01:39:42 +0530

    @AfterReturning updated

--- src/com/springaop/aopdemo/dao/AccountDAO.java
+++ src/com/springaop/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package com.springaop.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.springaop.aopdemo.Account;
@@ -7,6 +10,23 @@ import com.springaop.aopdemo.Account;
 @Component
 public class AccountDAO {
 
+	
+	public List<Account> findAccounts(){
+		
+		List<Account> myAccounts = new ArrayList<>();
+		
+		Account temp1 = new Account("Sai","Ashish");
+		Account temp2 = new Account("Sai1","Ashish1");
+		Account temp3 = new Account("Sai2","Ashish2");
+		
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+
+		return myAccounts;
+		
+	}
+	
 	public void addAccount(Account theAccount) {
 		System.out.println(getClass() + "Add Account" );
 	}
